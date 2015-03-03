36ce811c254c723dcb93554866a9ec5124e5d661
Rolandas Barysas <hello@rbrs.io>
2015-03-03 09:58:41 +0200

    get all streams status at once instead of looping through each channel

--- twitch.py
+++ twitch.py
@@ -3,37 +3,39 @@ import urllib2
 import os.path
 
 # Settings
-twitch_username = "your-twitch-username-here"
+twitch_username = "xrbrs"
 
 # Let's fetch and parse data from twitch
-r = urllib2.urlopen("https://api.twitch.tv/kraken/users/"+ twitch_username +"/follows/channels?limit=250").read()
+r = urllib2.urlopen("https://api.twitch.tv/kraken/users/"+ twitch_username +"/follows/channels?limit=100").read()
 raw_data = json.loads(r)
 
 output = ""
+
+# Get followed channels
+followed_channels = []
 for channel in raw_data["follows"]:
-	channel_name = channel["channel"]["name"]
-	
-	# Fetching data of individual channels
-	r = urllib2.urlopen("https://api.twitch.tv/kraken/streams/" + channel_name).read()
-	raw_channel_data = json.loads(r)
+  followed_channels.append(channel["channel"]["name"])
 
-	# We need only live channels
-	if raw_channel_data["stream"] != None:
-		channel_title = raw_channel_data["stream"]["channel"]["status"]
-		channel_game = raw_channel_data["stream"]["channel"]["game"]
+# Get live streams
+r = urllib2.urlopen("https://api.twitch.tv/kraken/streams?channel=%s" % ','.join(followed_channels))
+live_streams = json.loads(r.read())
 
-		output += "${color white}" + channel_name + " is ${color LawnGreen}LIVE\n${color white}"+channel_title+"\n${color yellow}"+channel_game+"\n\n"
+for stream in live_streams["streams"]:
+  channel_name = stream["channel"]["display_name"]
+  channel_title = stream["channel"]["status"]
+  channel_game = stream["channel"]["game"]
+  output += "${color white}" + channel_name + " is ${color LawnGreen}LIVE\n${color white}" + channel_title + "\n${color yellow}" + channel_game + "\n\n"
 
 # No live channels?
 if output == "":
-	output = "No live channels"
+   output = "No live channels"
 
 # Write output to file
 output = output.replace("#", "\#") # Preventing twitch titles from messing with conky config
 path = os.path.dirname(__file__) + "/"
 
 if path == "/":
-	path = ""
+  path = ""
 
 f = open(path + "streams.txt", "w")
 f.write(output.encode('utf8'))
